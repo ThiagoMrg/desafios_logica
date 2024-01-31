c68b6419466efc63bb56a24e9daf5d0514ee08e1
Thiago Mariano <thiago.m.developer@outlook.com>
2024-01-30 22:24:03 -0300

    Resolução desafio 1

---
+++ desafio00.py
@@ -0,0 +1,6 @@
+# Fácil  1 – Faça um programa que receba um número e usando laços de repetição calcule e mostre a tabuada desse número.
+
+num = int(input("Digite um número: "))
+
+for resultado in range(1,11):
+  print (num, "*", resultado, "=", resultado*num)
