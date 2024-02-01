3e95ba05ea12b549ad1f9219ea7f63657a88425c
ThiagoMrg <42078290+ThiagoMrg@users.noreply.github.com>
2024-01-31 22:41:51 -0300

    Add files via upload
    Resolução desafio 2

---
+++ desafio01.py
@@ -0,0 +1,6 @@
+# Fácil –  Faça um programa que mostre as tabuadas dos números de 1 a 10 usando laços de repetição.
+
+for i in range (1,11):
+    print("\n")
+    for j in range(1,10):
+        print(i, "*", j, "=", i*j)
